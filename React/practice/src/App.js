e5a8ef7ce9b5c52ffde401cd3810bbd0027744d4
suni21 <83060711+suni21@users.noreply.github.com>
2023-03-26 13:20:17 +0530

    new

---
+++ React/practice/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(){
+  return <h1>Hello world</h1>;
+}
+function App() {
+  return <Header></Header>;
+}
+
+export default App;
